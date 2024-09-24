c40d52d219f40157081b01c4f30c8d664f7025c6
soyamkapasiya <163526543+soyamkapasiya@users.noreply.github.com>
2024-09-24 18:04:32 +0530

    test update

--- src/main/java/com/kapasiya/journal_app/repository/RoleRepository.java
+++ src/main/java/com/kapasiya/journal_app/repository/RoleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends MongoRepository<Role,String> {
+public interface RoleRepository extends MongoRepository<Role, String> {
     Role findByName(String name);
 }
